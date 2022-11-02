977196f5d4e971f7fb1194717204b13ab5355519
Razzi65 <rizmoh65@gmail.com>
2022-11-03 01:52:22 +0500

    first push

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Button, Center, CircularProgress, CircularProgressLabel, Flex, Heading } from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react"
+import { Input } from '@chakra-ui/react'
+
+
+
+const Home: NextPage = () => {
+  return (
+    
+   <div>
+    
+   </div>
+  )
+}
+
+export default Home
